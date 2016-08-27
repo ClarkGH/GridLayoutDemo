f2c120431cc94e4c6b319c07872ae5c80df2b3c6
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-26 18:12:22 -0700

    Set up onclick method

--- app/src/main/java/com/example/clarkhinchcliff/gridlayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/gridlayoutdemo/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.clarkhinchcliff.gridlayoutdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void sayPhrase (View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
