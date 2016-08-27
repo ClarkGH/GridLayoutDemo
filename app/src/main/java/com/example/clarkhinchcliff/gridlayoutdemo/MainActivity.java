66b7153fee2cfc320a3cdfa127ed045d9742910e
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-26 18:24:36 -0700

    Can now say phrases

--- app/src/main/java/com/example/clarkhinchcliff/gridlayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/gridlayoutdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.clarkhinchcliff.gridlayoutdemo;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,13 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     public void sayPhrase (View view){
+        int id = view.getId();
+        String ourId;
+        ourId = view.getResources().getResourceEntryName(id);
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.clarkhinchcliff.gridlayoutdemo");
 
+        MediaPlayer mPlayer = MediaPlayer.create(this, resourceId);
+        mPlayer.start();
     }
 
     @Override
